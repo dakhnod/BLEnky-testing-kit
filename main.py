35b3a52f5788a5e09a2aac93a3496e2461c6e49e
Daniel Dakhno <dakhnod@gmail.com>
2023-12-11 02:23:10 +0100

    added simple test cases

--- main.py
+++ main.py
@@ -1,34 +1,44 @@
 import bleak
 import asyncio
 import math
+import argparse
+import RPi.GPIO as gpio
+import atexit
 
 class Tester():
-    def __init__(self) -> None:
-        self.device = bleak.BleakClient('1CF348D8-55AD-3E39-4F8F-679B29C7175E')
-        self.input_future = None
+    def __init__(self, address, input_pins, output_pins) -> None:
+        self.device = bleak.BleakClient(address)
+        self.ble_input_future = None
+        self.input_pins = input_pins
+        self.output_pins = output_pins
+
+    def gpio_setup(self):
+        gpio.setmode(gpio.BCM)
+        for input_pin in self.input_pins:
+            gpio.setup(input_pin, gpio.INPUT)
+        for output_pin in self.output_pins:
+            gpio.setup(output_pin, gpio.OUTPUT)
+
+        atexit.register(gpio.cleanup)
 
     async def run(self):
+        self.gpio_setup()
+
         await self.device.connect()
         
         def ble_input_handler(characteristic, data):
-            if self.input_future is None:
+            if self.ble_input_future is None:
                 return
-            self.input_future.set_result(self.decode_inputs(data))
+            self.ble_input_future.set_result(self.decode_inputs(data))
 
         await self.device.start_notify('00002a56-0000-1000-8000-00805f9b34fb', ble_input_handler)
 
-        print('waiting')
-
-        # await self.test_output(0, 1)
-        await self.test_outputs(0, )
-        print('test succeeded')
-
-        await asyncio.sleep(9999)
+        print('connected')
 
         while True:
-            await self.ble_send_outputs(1)
+            await self.ble_send_outputs([1])
             await asyncio.sleep(0.2)
-            await self.ble_send_outputs(0)
+            await self.ble_send_outputs([0])
             await asyncio.sleep(1)
 
     async def test_outputs(self, *outputs):
@@ -42,21 +52,24 @@ class Tester():
             raise RuntimeError('GPIO inputs not properly read in uC')
 
     async def test_input(self, index, expected_state):
-        self.input_future = asyncio.Future()
-        outputs = [expected_state] + [0b11] * index
-        self.gpio_set_outputs(index, expected_state)
+        self.ble_input_future = asyncio.Future()
+        self.gpio_set_output(index, expected_state)
         try:
-            async with asyncio.timeout(5):
-                inputs = await self.input_future
+            report = await asyncio.wait_for(self.ble_input_future, 5)
         except asyncio.TimeoutError:
             raise RuntimeError('uC did not detect/send changed input in time')
-        
-        self.input_future = None
-        if inputs[index] != expected_state:
+        finally:
+            self.ble_input_future = None
+
+        expected_report = [expected_state] + [0b11] * index
+        if report != expected_report:
             raise RuntimeError('uC output not matching expected')
 
     def gpio_set_output(self, index, output):
-        pass
+        gpio.output(self.output_pins[index], output)
+
+    def gpio_get_inputs(self):
+        return tuple(map(gpio.input, self.input_pins))
 
     def encode_outputs(self, outputs):
         data = [0xff] * math.ceil(len(outputs) / 4)
@@ -83,11 +96,14 @@ class Tester():
         data = await self.device.read_gatt_char('00002a56-0000-1000-8000-00805f9b34fb')
         return self.decode_inputs(data)
 
-    def gpio_get_inputs(self):
-        return (1, )
-
 async def main():
-    await Tester().run()
+    parser = argparse.ArgumentParser('BLEnky testing kit')
+    parser.add_argument('--address', '-a', type=str, required=True, help='BLE MAC address / Address UUID to connect to')
+    parser.add_argument('--input-pin', '-i', type=int, required=True, help='One or multiple input pins on the PI connected to the uC', action='append')
+    parser.add_argument('--output-pin', '-o', type=int, required=True, help='One or multiple output pins on the PI connected to the uC', action='append')
+    args = parser.parse_args()
+
+    await Tester(args.address, args.input_pin, args.output_pin).run()
 
 if __name__ == '__main__':
     asyncio.run(main())
