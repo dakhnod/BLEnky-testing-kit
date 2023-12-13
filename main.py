a3648d1c9da64e9c593ac726dce5416ffdd4c200
Daniel Dakhno <dakhnod@gmail.com>
2023-12-13 04:03:49 +0100

    made tests modular

--- main.py
+++ main.py
@@ -6,15 +6,41 @@ import RPi.GPIO as gpio
 import atexit
 import abc
 import inspect
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logging.StreamHandler.terminator = ''
+
+
+def print_color(color='reset'):
+    if color is None:
+        print("\x1b[0m")
+        return
+    print({
+        'grey': "\x1b[38;20m",
+        'yellow': "\x1b[33;20m",
+        'red': "\x1b[31;20m",
+        'green': "\x1b[32;20m",
+        'bold_red': "\x1b[31;1m",
+        'reset': "\x1b[0m",
+    }[color], end='')
 
 class IOLayer(abc.ABC):
     def __init__(self, name):
         self.name = name
+    def init(self):
+        pass
     def uninit(self):
         pass
     def set_outputs(self, outputs):
         pass
     def get_inputs(self):
+        pass
+    def set_output(self, index, output):
+        pass
+    def get_input(self, index):
+        pass
+    def before_get_input(self, index):
         pass
 
 class RPIGPIOLayer(IOLayer):
@@ -22,6 +48,8 @@ class RPIGPIOLayer(IOLayer):
         super().__init__('GPIO')
         self.input_pins = input_pins
         self.output_pins = output_pins
+
+    async def init(self):
         gpio.setmode(gpio.BCM)
         for input_pin in self.input_pins:
             gpio.setup(input_pin, gpio.IN)
@@ -35,20 +63,32 @@ class RPIGPIOLayer(IOLayer):
         gpio.output(self.output_pins[:len(outputs)], list(map(bool, outputs))) 
 
     def get_inputs(self):
-        return gpio.input(self.input_pins)
+        return tuple(map(gpio.input, self.input_pins))
+    
+    def set_output(self, index, output):
+        gpio.output(self.output_pins[index], output)
+
+    def get_input(self, index):
+        return gpio.input(self.input_pins[index])
     
 class BlenkyLayer(IOLayer):
-    async def __init__(self, address):
-        super().__init__("BLE")
+    def __init__(self, address):
+        super().__init__('BLE')
         self.device = bleak.BleakClient(address)
         self.ble_input_future = None
+        self.logger = logging.getLogger('BLE')
+        self.logger.addHandler(logging.StreamHandler())
+        self.logger.handlers[0].terminator = '\n'
+
+    async def init(self):
         await self.connect()
 
         def ble_input_handler(characteristic, data):
+            inputs = self._decode_inputs(data)
             if self.ble_input_future is None:
                 return
-            inputs = self.decode_inputs(data)
-            print(f'new inputs: {inputs}')
+            if self.ble_input_future.done():
+                return
             self.ble_input_future.set_result(inputs)
 
         await self.device.start_notify('00002a56-0000-1000-8000-00805f9b34fb', ble_input_handler)
@@ -58,7 +98,11 @@ class BlenkyLayer(IOLayer):
         await self.device.disconnect()
 
     async def connect(self):
+        self.logger.info('connecting...')
         await self.device.connect()
+        print_color('green')
+        print('OK')
+        print_color()
 
     def _encode_outputs(self, outputs):
         data = [0xff] * math.ceil(len(outputs) / 4)
@@ -81,83 +125,121 @@ class BlenkyLayer(IOLayer):
     async def set_outputs(self, outputs):
         await self.device.write_gatt_char('00002a57-0000-1000-8000-00805f9b34fb', self._encode_outputs(outputs))
 
+    async def before_get_input(self, index):
+        self.ble_input_future = asyncio.Future()
+
     async def get_inputs(self):
         data = await self.device.read_gatt_char('00002a56-0000-1000-8000-00805f9b34fb')
-        return self._decode_inputs(data)
+
+        read = self._decode_inputs(data)
+        return read
+    
+    async def set_output(self, index, output):
+        outputs = [0b11] * index + [output]
+        await self.device.write_gatt_char('00002a57-0000-1000-8000-00805f9b34fb', self._encode_outputs(outputs))
+
+    async def get_input(self, index):
+        try:
+            reported = await asyncio.wait_for(self.ble_input_future, 5)
+            return reported[index]
+        except asyncio.exceptions.TimeoutError:
+            self.logger.error('Error waiting for input notification')
+            return None
 
 class Tester():
-    async def call_func(func, *args, **kwargs):
+    async def call_func(self, func, *args, **kwargs):
         if inspect.iscoroutinefunction(func):
             return await func(*args, **kwargs)
         return func(*args, **kwargs)
     
-    async def __init__(self, *layers) -> None:
+    def __init__(self, *layers) -> None:
         if(len(layers) != 2):
             raise NotImplementedError('Only 2 layers supported')
         self.layers = layers
-        for layer in layers:
+        self.logger = logging.getLogger('Tests')
+
+    async def init(self):
+        for layer in self.layers:
             await self.call_func(layer.init)
 
+    class TestFailedError(RuntimeError):
+        pass
+
     async def run(self):
-
-        print('connected')
-
-        await self.test_multiple_inputs((0,))
-        await self.test_multiple_inputs((1,))
-        await self.test_multiple_inputs((0,))
-        await self.test_multiple_inputs((1,))
-
-        await self.test_input(0, 0)
-        await self.test_input(0, 1)
-        await self.test_input(0, 0)
-        await self.test_input(0, 1)
-
-        await self.test_outputs((0,))
-        await self.test_outputs((1,))
-        await self.test_outputs((0,))
-        await self.test_outputs((1,))
-
-    async def test_outputs(self, outputs):
-        outputs = tuple(outputs)
-        await self.ble_send_outputs(outputs)
-        gpio_inputs = self.gpio_get_inputs()
-        print(f'expected outputs {outputs}, reported: {gpio_inputs}')
-        if gpio_inputs != outputs:
-            raise RuntimeError('GPIO outputs not properly set in uC')
-
-    async def test_multiple_inputs(self, inputs):
-        inputs = tuple(inputs)
-        self.gpio_set_outputs(inputs)
-        # await asyncio.sleep(1)
-        reported = await self.ble_read_inputs()
-        print(f'expected: {inputs}, reported: {reported}')
-        if inputs != reported:
-            raise RuntimeError('GPIO inputs not properly read in uC')
-
-    async def test_input(self, index, expected_state):
-        print(f'testing input #{index}: {expected_state}')
-        self.ble_input_future = asyncio.Future()
-        self.gpio_set_output(index, expected_state)
+        signals_list = (
+            (0,0,0,0),
+            (1,0,0,0),
+            (0,1,0,0),
+            (0,0,1,0),
+            (0,0,0,1),
+            (1,1,0,0),
+            (0,0,1,1),
+            (1,0,1,0),
+            (0,1,0,1),
+            (1,1,1,1),
+            (0,0,0,0),
+        )
+        test_count = [0,0]
         try:
-            report = await asyncio.wait_for(self.ble_input_future, 5)
-        except asyncio.TimeoutError:
-            raise RuntimeError('uC did not detect/send changed input in time')
-        finally:
-            self.ble_input_future = None
+            for layers in (self.layers, self.layers[::-1]):
+                for signals in signals_list:
+                    try:
+                        self.logger.info(f'testing signals {signals}, {layers[0].name} -> {layers[1].name}...')
+                        await self.test_signals(layers, signals)
+                        print_color('green')
+                        print('OK')
+                        print_color()
+                        test_count[0] += 1
+                    except self.TestFailedError as e:
+                        print()
+                        print_color('red')
+                        self.logger.error(str(e))
+                        print_color()
+                        print()
+                        test_count[1] += 1
+            # for layer in self.layers:
+            #     await self.call_func(layer.set_outputs, (0, 0, 0, 0))
 
-        expected_report = tuple([expected_state] + [0b11] * index)
-        if report != expected_report:
-            print(f'expected: {expected_report}, actual: {report}')
-            raise RuntimeError('uC output not matching expected')
-    
-    def decode_inputs(self, input_data):
-        inputs = []
-        for byte in input_data:
-            for i in range(0, 8, 2):
-                inputs.append((byte >> i) & 0b11)
-        while len(inputs) > 0 and inputs[-1] == 0b11:
-            inputs.pop(-1)
-        return tuple(inputs)
+            for layers in (self.layers, self.layers[::-1]):
+                for signal in (1, 0):
+                    for index in range(4):
+                        try:
+                            self.logger.info(f'testing signal index {index}: {signal}, {layers[0].name} -> {layers[1].name}...')
+                            await self.test_signal(layers, index, signal)
+                            print_color('green')
+                            print('OK')
+                            print_color()
+                            test_count[0] += 1
+                        except self.TestFailedError as e:
+                            print()
+                            print_color('red')
+                            self.logger.error(str(e))
+                            print_color()
+                            print()
+                            test_count[1] += 1
+        finally:
+            for layer in self.layers:
+                await self.call_func(layer.uninit)
+        if test_count[1] == 0:
+            print_color('green')
+        else:
+            print_color('red')
+        self.logger.info(f'Tests successful: %d, failed: %d', *test_count)
+        print_color()
+        print()
+
+    async def test_signals(self, layers, signals: tuple[int]):
+        await self.call_func(layers[0].set_outputs, signals)
+        reported = await self.call_func(layers[1].get_inputs)
+        if signals != reported:
+            raise self.TestFailedError(f'signal {layers[0].name}{signals} did not match {layers[1].name}{reported}')
+
+    async def test_signal(self, layers, index, signal):
+        await self.call_func(layers[1].before_get_input, index)
+        await self.call_func(layers[0].set_output, index, signal)
+        reported = await self.call_func(layers[1].get_input, index)
+        if signal != reported:
+            raise self.TestFailedError(f'signal {layers[0].name}: {signal} did not match {layers[1].name}: {reported}')
 
 async def main():
     parser = argparse.ArgumentParser('BLEnky testing kit')
@@ -166,10 +248,12 @@ async def main():
     parser.add_argument('--output-pin', '-o', type=int, required=True, help='One or multiple output pins on the PI connected to the uC', action='append')
     args = parser.parse_args()
 
-    gpioLayer = await RPIGPIOLayer(args.input_pin, args.output_pin)
-    bleLayer = await BlenkyLayer(args.address)
+    gpioLayer = RPIGPIOLayer(args.input_pin, args.output_pin)
+    bleLayer = BlenkyLayer(args.address)
 
-    await Tester(args.address, args.input_pin, args.output_pin).run()
+    tester = Tester(gpioLayer, bleLayer)
+    await tester.init()
+    await tester.run()
 
 if __name__ == '__main__':
     asyncio.run(main())
